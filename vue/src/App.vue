fcaabaaeb5d4594e2b4d5b43ed480a6adf94db48
xiaoqiuuuu <2365164682@qq.com>
2024-03-03 16:01:26 +0800

    第一个 vue 界面

---
+++ vue/src/App.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Person from "./components/Person.vue";
+
+  export default {
+    name: 'App',
+    components:{Person}
+  }
+</script>
+
+<template>
+  <div class="app">
+    <h1>你好呀，我的第一个vue界面!</h1>
+    <Person/>
+  </div>
+</template>
+
+
+<style>
+  .app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
---
+++ vue/src/components/Person.vue
@@ -0,0 +1,59 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Person",
+  data() {
+    return {
+      name: "张三",
+      age:18,
+      tel:18123456789,
+      email:"123@qq.com",
+      address:"北京市朝阳区"
+    }
+  },
+  methods:{
+    modifyName(){
+      if (this.name=="张三"){
+          this.name="李四";
+      } else {
+        this.name="张三";
+      }
+    },
+    modifyAge(){
+      this.age+=1;
+    },
+    showTle(){
+      alert(this.tel);
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="person">
+    <h1>姓名：{{name}}</h1>
+    <h1>年龄：{{age}}</h1>
+    <h1>邮箱：{{email}}</h1>
+    <h1>地址：{{address}}</h1>
+    <button @click="modifyName">修改姓名</button>
+    <button @click="modifyAge">修改年龄</button>
+    <button @click="showTle">点击查看电话</button>
+  </div>
+
+</template>
+
+<style scoped>
+  .person{
+    background-color: skyblue;
+    box-shadow:0 0 10px rgba(0,0,0,0.5);
+    border-radius: 10px;
+    padding: 20px;
+  }
+
+  button{
+    margin: 0 10px;
+  }
+
+
+</style>
